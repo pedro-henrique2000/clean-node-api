a46289e1de96a273ec1a3e6a5e8df8f8a7c4e5f3
phb2000 <p.becker2000@hotmail.com>
2022-04-29 15:48:23 -0300

    ensure that 400 is returned if no password is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route(httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400,
       };
@@ -14,6 +14,17 @@ describe("Login Router", () => {
     const httpRequest = {
       body: {
         password: "123",
+      },
+    };
+    const response = sut.route(httpRequest);
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("Should return 400 if no password is provided", () => {
+    const sut = new LoginRouter();
+    const httpRequest = {
+      body: {
+        email: "anyEmail",
       },
     };
     const response = sut.route(httpRequest);
