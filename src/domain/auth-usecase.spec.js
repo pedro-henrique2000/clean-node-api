e3da8b775301e2f70b67956a5b0182510743d482
phb2000 <p.becker2000@hotmail.com>
2022-04-30 20:36:53 -0300

    Ensure that error occurs if no password is provided

--- src/domain/auth-usecase.spec.js
+++ src/domain/auth-usecase.spec.js
@@ -5,6 +5,10 @@ class AuthUseCase {
     if (!email) {
       throw new MissingParamError('email');
     }
+
+    if (!password) {
+      throw new MissingParamError('password');
+    }
   }
 }
 
@@ -13,5 +17,11 @@ describe("AuthUseCase", () => {
     const sut = new AuthUseCase();
     const promise = sut.auth();
     expect(promise).rejects.toThrow(new MissingParamError('email'));
+  });
+
+  it("Should Throw Error If null Password Is Provided", () => {
+    const sut = new AuthUseCase();
+    const promise = sut.auth('anyMail');
+    expect(promise).rejects.toThrow(new MissingParamError('password'));
   });
 });
