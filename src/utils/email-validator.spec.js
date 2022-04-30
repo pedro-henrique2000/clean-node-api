bfd065156974e9d33179a1dd8d02ce58ba951697
phb2000 <p.becker2000@hotmail.com>
2022-04-30 16:55:21 -0300

    creating email validator factory

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -6,15 +6,19 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator();
+};
+
 describe("Email Validator", () => {
   it("Should return true if validator returns true", () => {
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isValid = sut.isValid("valid_email@email.com");
     expect(isValid).toBe(true);
   });
 
   it("Should return false if validator returns false", () => {
-    const sut = new EmailValidator();
+    const sut = makeSut();
     validator.isEmailValid = false;
     const isValid = sut.isValid("invalid_email@mail.com");
     expect(isValid).toBe(false);
