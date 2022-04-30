382f2843343a489ff60903f1cf14f186d1e3f539
phb2000 <p.becker2000@hotmail.com>
2022-04-30 20:35:47 -0300

    ensure that missingParamError is Throwed if email is invalid

--- src/domain/auth-usecase.spec.js
+++ src/domain/auth-usecase.spec.js
@@ -1,15 +1,17 @@
+const {MissingParamError} = require('../utils/errors')
+
 class AuthUseCase {
   async auth(email, password) {
     if (!email) {
-      throw new Error();
+      throw new MissingParamError('email');
     }
   }
 }
 
 describe("AuthUseCase", () => {
-  it("Should Throw Error If null Email Is Provided", async () => {
+  it("Should Throw Error If null Email Is Provided", () => {
     const sut = new AuthUseCase();
     const promise = sut.auth();
-    expect(promise).reject.toThrow();
+    expect(promise).rejects.toThrow(new MissingParamError('email'));
   });
 });
