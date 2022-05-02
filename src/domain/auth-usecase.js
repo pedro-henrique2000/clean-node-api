e91c3b9524f455df15dcb0b161a4d0ffe9a3bbc3
phb2000 <p.becker2000@hotmail.com>
2022-05-02 11:33:57 -0300

    refactor auth user

--- src/domain/auth-usecase.js
+++ src/domain/auth-usecase.js
@@ -16,14 +16,13 @@ module.exports = class AuthUseCase {
     }
 
     const user = await this.loadUserByEmailRepository.load(email);
-    if (!user) return null;
+    const isValid = user && await this.encrypter.compare(password, user.password);
 
-    const isValid = await this.encrypter.compare(password, user.password);
+    if (isValid) {
+      const accessToken = await this.tokenGenerator.generate(user.id);
+      return accessToken;
+    }
 
-    if (!isValid) return null;
-
-    const accessToken = await this.tokenGenerator.generate(user.id);
-
-    return accessToken;
+    return null;
   }
 };
