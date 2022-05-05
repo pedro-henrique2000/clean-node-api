ec5f7a24d9074ca5b4525e326b913f8f9c22cc6b
Pedro Henrique Arnold Becker <p.becker2000@hotmail.com>
2022-05-05 17:47:45 -0300

    Auth Use Case Adjusts

--- src/domain/auth-usecase.spec.js
+++ src/domain/auth-usecase.spec.js
@@ -14,6 +14,16 @@ const makeTokenGenerator = () => {
   return tokenGeneratorSpy;
 };
 
+const makeTokenGeneratorWithError = () => {
+  class TokenGenerator {
+    async generate(userId) {
+      throw new Error();
+    }
+  }
+
+  return new TokenGenerator();
+};
+
 const makeEncrypter = () => {
   class EncrypterSpy {
     async compare(password, hashedPassword) {
@@ -27,6 +37,16 @@ const makeEncrypter = () => {
   encrypterSpy.isValid = true;
 
   return encrypterSpy;
+};
+
+const makeEncrypterWithError = () => {
+  class EncrypterSpy {
+    async compare(password, hashedPassword) {
+      throw new Error();
+    }
+  }
+
+  return new EncrypterSpy();
 };
 
 const makeLoadUserByEmailRepositorySpy = () => {
@@ -45,16 +65,46 @@ const makeLoadUserByEmailRepositorySpy = () => {
   return loadUserByEmailRepositorySpy;
 };
 
+const makeLoadUserByEmailRepositorySpyWithError = () => {
+  class LoadUserByEmailRepositorySpy {
+    async load(email) {
+      throw new Error();
+    }
+  }
+  return new LoadUserByEmailRepositorySpy();
+}
+
+const makeUpdateAccessTokenRepository = () => {
+  class UpdateAccessTokenRepositorySpy {
+    async update(userId, accessToken) {
+      this.userId = userId;
+      this.accessToken = accessToken;
+    }
+  }
+  return new UpdateAccessTokenRepositorySpy();
+}
+
+const makeUpdateAccessTokenRepositoryWithError = () => {
+  class UpdateAccessTokenRepositorySpy {
+    async update(userId, accessToken) {
+      throw new Error()
+    }
+  }
+  return new UpdateAccessTokenRepositorySpy();
+}
+
 const makeSut = () => {
   const encrypterSpy = makeEncrypter();
   const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepositorySpy();
+  const updateAccessTokenRepositorySpy = makeUpdateAccessTokenRepository();
   const tokenGeneratorSpy = makeTokenGenerator();
   const sut = new AuthUseCase({
     loadUserByEmailRepository: loadUserByEmailRepositorySpy,
+    updateAccessTokenRepository: updateAccessTokenRepositorySpy,
     encrypter: encrypterSpy,
     tokenGenerator: tokenGeneratorSpy,
   });
-  return { sut, loadUserByEmailRepositorySpy, encrypterSpy, tokenGeneratorSpy };
+  return { sut, loadUserByEmailRepositorySpy, updateAccessTokenRepositorySpy, encrypterSpy, tokenGeneratorSpy };
 };
 
 describe("AuthUseCase", () => {
@@ -105,6 +155,13 @@ describe("AuthUseCase", () => {
     expect(accessToken).toBeTruthy();
   });
 
+  it("Should return an accessToken if correct credentials are provided", async () => {
+    const { sut, loadUserByEmailRepositorySpy, updateAccessTokenRepositorySpy, tokenGeneratorSpy } = makeSut();
+    await sut.auth("anyEmail@email.com", "anyPassword");
+    expect(updateAccessTokenRepositorySpy.userId).toBe(loadUserByEmailRepositorySpy.user.id);
+    expect(updateAccessTokenRepositorySpy.accessToken).toBe(tokenGeneratorSpy.accessToken);
+  });
+
   it("Should Throw if invalid dependecies are provided", async () => {
     const invalid = {}
     const suts = [].concat(
@@ -117,12 +174,36 @@ describe("AuthUseCase", () => {
       new AuthUseCase({ loadUserByEmailRepository: makeLoadUserByEmailRepositorySpy(), encrypter: makeEncrypter() }),
       new AuthUseCase({ loadUserByEmailRepository: makeLoadUserByEmailRepositorySpy(), encrypter: makeEncrypter(), tokenGenerator: null }),
       new AuthUseCase({ loadUserByEmailRepository: makeLoadUserByEmailRepositorySpy(), encrypter: makeEncrypter(), tokenGenerator: invalid }),
-      new AuthUseCase({ loadUserByEmailRepository: makeLoadUserByEmailRepositorySpy(), encrypter: makeEncrypter(), tokenGenerator: makeTokenGenerator() }),
+      new AuthUseCase({ loadUserByEmailRepository: makeLoadUserByEmailRepositorySpy(), encrypter: makeEncrypter(), tokenGenerator: makeTokenGeneratorWithError() }),
+      new AuthUseCase({
+        loadUserByEmailRepository: makeLoadUserByEmailRepositorySpy(), encrypter: makeEncrypter(), tokenGenerator: makeTokenGenerator()
+      }),
+      new AuthUseCase({
+        loadUserByEmailRepository: makeLoadUserByEmailRepositorySpy(), encrypter: makeEncrypter(), tokenGenerator: makeTokenGenerator(), updateAccessTokenRepository: null
+      }),
+      new AuthUseCase({
+        loadUserByEmailRepository: makeLoadUserByEmailRepositorySpy(), encrypter: makeEncrypter(), tokenGenerator: makeTokenGenerator(), updateAccessTokenRepository: invalid
+      })
     )
     for (const sut of suts) {
       const promise = sut.auth("anyMail", "anyPassword");
       expect(promise).rejects.toThrow();
-      
+
+    }
+  });
+
+  it("Should Throw if a dependecie throws an error", async () => {
+    const suts = [].concat(
+      new AuthUseCase({ loadUserByEmailRepository: makeLoadUserByEmailRepositorySpyWithError(), encrypter: makeEncrypter(), tokenGenerator: makeTokenGenerator() }),
+      new AuthUseCase({ loadUserByEmailRepository: makeLoadUserByEmailRepositorySpy(), encrypter: makeEncrypterWithError(), tokenGenerator: makeTokenGenerator() }),
+      new AuthUseCase({ loadUserByEmailRepository: makeLoadUserByEmailRepositorySpy(), encrypter: makeEncrypter(), tokenGenerator: makeTokenGeneratorWithError() }),
+      new AuthUseCase({ loadUserByEmailRepository: makeLoadUserByEmailRepositorySpy(), encrypter: makeEncrypter(), tokenGenerator: makeTokenGenerator() }),
+      new AuthUseCase({ loadUserByEmailRepository: makeLoadUserByEmailRepositorySpy(), encrypter: makeEncrypter(), tokenGenerator: makeTokenGenerator(), updateAccessTokenRepository: makeUpdateAccessTokenRepositoryWithError() })
+
+    )
+    for (const sut of suts) {
+      const promise = sut.auth("anyMail@mail.com", "anyPassword");
+      expect(promise).rejects.toThrow();
     }
   });
 });
