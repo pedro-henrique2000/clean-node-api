ddec7909650cbcdcd9039b86ddad7ff908bd44e6
phb2000 <p.becker2000@hotmail.com>
2022-05-07 15:38:26 -0300

    created helper factory method

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,22 +1,27 @@
-const jwt = require('jsonwebtoken') 
+const jwt = require("jsonwebtoken");
 
 class TokenGenerator {
-    async generate(id) {
-        return jwt.sign(id, 'secret');
-    }
+  async generate(id) {
+    return jwt.sign(id, "secret");
+  }
 }
 
-describe('Token Generator', () => {
-    it('Should return null if JWT return null', async () => {
-        const sut = new TokenGenerator();
-        jwt.token = null;
-        const token = await sut.generate('any_id');
-        expect(token).toBeNull();
-    })
+const makeSut = () => {
+  const tokenGen = new TokenGenerator();
+  return tokenGen;
+};
 
-    it('Should return token if JWT returns a token', async () => {
-        const sut = new TokenGenerator();
-        const token = await sut.generate('any_id');
-        expect(token).toBe(jwt.token);
-    })
-})
+describe("Token Generator", () => {
+  it("Should return null if JWT return null", async () => {
+    const sut = makeSut();
+    jwt.token = null;
+    const token = await sut.generate("any_id");
+    expect(token).toBeNull();
+  });
+
+  it("Should return token if JWT returns a token", async () => {
+    const sut = makeSut();
+    const token = await sut.generate("any_id");
+    expect(token).toBe(jwt.token);
+  });
+});
