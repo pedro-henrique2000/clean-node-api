ec5ef3e40f92eb71cf29e6184695767f706ac6a5
phb2000 <p.becker2000@hotmail.com>
2022-05-07 15:35:52 -0300

    ensure that token generator returns expected values

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,13 +1,22 @@
+const jwt = require('jsonwebtoken') 
+
 class TokenGenerator {
     async generate(id) {
-        return null;
+        return jwt.sign(id, 'secret');
     }
 }
 
 describe('Token Generator', () => {
     it('Should return null if JWT return null', async () => {
         const sut = new TokenGenerator();
+        jwt.token = null;
         const token = await sut.generate('any_id');
         expect(token).toBeNull();
+    })
+
+    it('Should return token if JWT returns a token', async () => {
+        const sut = new TokenGenerator();
+        const token = await sut.generate('any_id');
+        expect(token).toBe(jwt.token);
     })
 })
