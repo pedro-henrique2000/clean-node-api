99be1577454ae79b0e0b3fc6d9b31ffd76ddee46
phb2000 <p.becker2000@hotmail.com>
2022-05-07 23:29:51 -0300

    adding dependecies and params validations

--- src/infra/repositories/update-user-token-repository.spec.js
+++ src/infra/repositories/update-user-token-repository.spec.js
@@ -6,6 +6,13 @@ class UpdateUserTokenRepository {
     this.userModel = userModel;
   }
   async update(userId, accessToken) {
+    if (!userId) {
+      throw new MissingParamError("userId");
+    }
+
+    if (!accessToken) {
+      throw new MissingParamError("accessToken");
+    }
     await this.userModel.updateOne({ _id: userId }, { $set: { accessToken } });
   }
 }
@@ -44,7 +51,37 @@ describe("Update User Token Repository", () => {
       password: hashedPassword,
     });
     await sut.update(createdUser.insertedId, "valid_token");
-    const updatedUser = await userModel.findOne({ _id: createdUser.insertedId });
+    const updatedUser = await userModel.findOne({
+      _id: createdUser.insertedId,
+    });
     expect(updatedUser.accessToken).toBe("valid_token");
+  });
+
+  test(`should throw if no userId is provided`, async () => {
+    const { sut } = makeSut();
+    const promise = sut.update(null, "valid_token");
+    expect(promise).rejects.toThrow(new MissingParamError("userId"));
+  });
+
+  test(`should throw if no accessToken is provided`, async () => {
+    const { sut } = makeSut();
+    const promise = sut.update("valid_id", null);
+    expect(promise).rejects.toThrow(new MissingParamError("accessToken"));
+  });
+
+  it(`should throw if no userModel is provided`, async () => {
+    const userModel = db.collection("users");
+    const sut = new UpdateUserTokenRepository();
+    const hashedPassword = "hashed_password";
+    const email = "valid_mail@mail.com";
+    const createdUser = await userModel.insertOne({
+      email,
+      name: "any_name",
+      age: 50,
+      state: "any",
+      password: hashedPassword,
+    });
+    const promise = sut.update(createdUser.insertedId, "valid_token");
+    expect(promise).rejects.toThrow();
   });
 });
