f8adf011a06268d101eddde866f37a71f2e3dbc0
phb2000 <p.becker2000@hotmail.com>
2022-05-07 22:47:17 -0300

    adding mongo helper tests

---
+++ src/infra/helpers/mongo-helper.spec.js
@@ -0,0 +1,18 @@
+const MongoHelper = require("./mongo-helper");
+
+describe("MongoHelper", () => {
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test("Should reconnect if no connect if client is null", async () => {
+    await MongoHelper.connect(process.env.MONGO_URL, null);
+    expect(MongoHelper.db).toBeDefined();
+    await MongoHelper.disconnect();
+    expect(MongoHelper.db).toBeNull();
+    expect(MongoHelper.client).toBeNull();
+    await MongoHelper.getDb();
+    expect(MongoHelper.db).toBeTruthy();
+    expect(MongoHelper.client).toBeTruthy();
+  });
+});
