99004a28d3cd16b83f84cebbbec0c19aa95a584c
phb2000 <p.becker2000@hotmail.com>
2022-05-09 20:25:02 -0300

    ensure res returns json content type

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,7 +1,10 @@
 const cors = require("../middleware/cors");
 const expressJson = require("../middleware/json-parser");
+const contentType = require("../middleware/content-type");
+
 module.exports = (app) => {
   app.disable("x-powered-by");
   app.use(cors);
   app.use(expressJson);
+  app.use(contentType);
 };
---
+++ src/main/middleware/content-type.test.js
@@ -0,0 +1,12 @@
+const request = require("supertest");
+const app = require("../config/app");
+
+describe("Content Type", () => {
+  it("should return json content type as default", async () => {
+    app.get("/test_content_type", (req, res) => {
+      res.send({});
+    });
+
+    await request(app).get("/test_content_type").expect("content-type", /json/);
+  });
+});
