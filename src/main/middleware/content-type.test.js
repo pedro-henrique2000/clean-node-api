ee698da55ba51e48a765a0f616c0900d020523cf
phb2000 <p.becker2000@hotmail.com>
2022-05-10 16:38:14 -0300

    cleaning jest modules

--- src/main/middleware/content-type.test.js
+++ src/main/middleware/content-type.test.js
@@ -1,7 +1,11 @@
 const request = require("supertest");
-const app = require("../config/app");
-
+let app 
 describe("Content Type", () => {
+  beforeEach(() => {
+    jest.resetModules()
+    app = require("../config/app");
+  })
+
   it("should return json content type as default", async () => {
     app.get("/test_content_type", (req, res) => {
       res.send({});
@@ -11,11 +15,11 @@ describe("Content Type", () => {
   });
 
   it("should return text content type if forced", async () => {
-    app.get("/test_content_type_xml", (req, res) => {
+    app.get("/test_content_type", (req, res) => {
       res.type('xml')
       res.send('');
     });
 
-    await request(app).get("/test_content_type_xml").expect("content-type", /xml/);
+    await request(app).get("/test_content_type").expect("content-type", /xml/);
   });
 });
