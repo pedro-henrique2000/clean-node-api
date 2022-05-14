a8a2e17bb14220b45eb5157c7fccf64129c5198d
phb2000 <p.becker2000@hotmail.com>
2022-05-14 14:49:23 -0300

    fixing routes

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const setupApp = require(`./setup`);
+const setupRoutes = require(`../routes/login-routes`)
 setupApp(app);
+setupRoutes(app)
 
 module.exports = app;
---
+++ src/main/routes/login-routes.spec.js
@@ -0,0 +1,38 @@
+const request = require("supertest");
+const app = require(`../config/app`);
+const MongoHelper = require("../../infra/helpers/mongo-helper");
+
+let userModel;
+
+describe("Login Routes", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL, null);
+    userModel = await MongoHelper.getCollection("users");
+  });
+
+  beforeEach(async () => {
+    await userModel.deleteMany();
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  it("should return 200 when valid credentials are provided", async () => {
+    const user = await userModel.insertOne({
+      email: "email@mail.com",
+      name: "any_name",
+      age: 50,
+      state: "any",
+      password: "hashedPassword",
+    });
+
+    await request(app)
+      .post(`/api/login`)
+      .send({
+        email: "email@mail.com",
+        password: "hashedPassword",
+      })
+      .expect(200);
+  });
+});
